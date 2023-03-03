31529ad954f9257bd4226dfaa004bb17cfc55418
Run396 <Run396@users.noreply.github.com>
2023-03-03 11:00:47 +0100

    Cleaned up

--- Test code/KmeansClusteringOnPixelValues.py
+++ Test code/KmeansClusteringOnPixelValues.py
@@ -1,19 +1,16 @@
 import torch
-import pandas as pd
 import numpy as np
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
-import numpy
 import warnings
-warnings.filterwarnings("ignore")
 from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 from sklearn.cluster import MiniBatchKMeans
 
-
+warnings.filterwarnings("ignore")
 
 # Download training data from open datasets.
 train_data = datasets.MNIST(
@@ -44,19 +41,15 @@ test_taget_numpyList = []
 
 x = 0
 for x in train_purified:
-
     train_numpyList.append(x.numpy())
 
 for x in test_purified:
-
     test_numpyList.append(x.numpy())
 
 
 #convert targets to NP_arrays
 train_target_numpyArray = np.array(train_target)
 test_target_numpyArray = np.array(test_target)
-
-
 
 train_numpyArray=np.array(train_numpyList)
 train_numpyArray= train_numpyArray.squeeze()
