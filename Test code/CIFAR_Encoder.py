fb08c462304d7d0898ccbad443614c69eab04fe1
Run396 <Run396@users.noreply.github.com>
2023-03-03 16:05:19 +0100

    name changes

--- Test code/CIFAR_Encoder.py
+++ Test code/CIFAR_Encoder.py
@@ -50,7 +50,7 @@ class ConvAutoencoder(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1)  
         self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-       
+        
         #Decoder
         self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
         self.t_conv2 = nn.ConvTranspose2d(16, 3, 2, stride=2)
