674a2fd249a8dedbffb302547aeb8f6ef131f7a4
Nathan Reece <nreece20@student.aau.dk>
2023-03-07 11:28:40 +0100

    Cleanup of supurflous function parameters

--- KmeansClustering/Main.py
+++ KmeansClustering/Main.py
@@ -10,8 +10,8 @@ def main():
     print("\nProgram initialized")
     warnings.filterwarnings("ignore")
     train_data, test_data = func.downloadData(datasets.MNIST)
-    train_reshaped, train_target_numpyArray, test_reshaped, test_target_numpyArray, train_numpyArray, test_numpyArray = func.purifyData(train_data, test_data)
-    accuracy_list, time_elapsed_list, kmeans_list = func.runProgram(train_data, test_data, train_reshaped, train_target_numpyArray, test_reshaped, test_target_numpyArray, train_numpyArray, test_numpyArray)
+    train_reshaped, train_target_numpyArray = func.purifyData(train_data, test_data)
+    accuracy_list, time_elapsed_list, kmeans_list = func.runProgram(train_reshaped, train_target_numpyArray)
     func.statsPrint(accuracy_list, time_elapsed_list, kmeans_list)
 
     func.saveToCSV(accuracy_list, time_elapsed_list)
