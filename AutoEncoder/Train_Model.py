e80c29ae6282625310ac04e483e39a6edb7f8ad7
Run396 <Run396@users.noreply.github.com>
2023-03-17 09:20:40 +0100

    Update Train_Model.py

--- AutoEncoder/Train_Model.py
+++ AutoEncoder/Train_Model.py
@@ -4,10 +4,21 @@ import torch.nn as nn
 import torch.utils.data as data
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import time 
+
+start = time.time()
+# set device
+if torch.cuda.is_available():
+    print('Using GPU')
+    dtype = torch.float32
+    device = torch.device('cuda:0')
+else:
+    print('Using CPU')
+    device = torch.device('cpu')
 
 # Settings
-epochs = 10
-batch_size = 128
+epochs = 5
+batch_size = 256
 lr = 0.01
 
 # DataLoader
@@ -57,6 +68,7 @@ class AutoEncoder(nn.Module):
 
 # Optimizer and loss function
 model = AutoEncoder()
+model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 loss_function = nn.MSELoss()
 
@@ -65,7 +77,7 @@ loss_function = nn.MSELoss()
 for epoch in range(epochs):
     for data, labels in train_loader:
         inputs = data.view(-1, 784)
-
+        inputs = inputs.to(device)
         # Forward
         codes, decoded = model(inputs)
 
@@ -77,6 +89,9 @@ for epoch in range(epochs):
 
     # Show progress
     print('[{}/{}] Loss:'.format(epoch+1, epochs), loss.item())
+
+print('Finished Training using', device)
+print('Time: ', time.time() - start)
 
 # Save
 torch.save(model, 'autoencoder.pth')
