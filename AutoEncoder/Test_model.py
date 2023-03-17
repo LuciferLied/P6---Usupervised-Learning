d777bad6d506de7f534b35b5d836c534024d5c41
Run396 <Run396@users.noreply.github.com>
2023-03-17 10:03:23 +0100

    Revert "Update Test_model.py"
    This reverts commit 731c37d62edc7015ee6895d04acfa5a2b877640a.

--- AutoEncoder/Test_model.py
+++ AutoEncoder/Test_model.py
@@ -13,19 +13,15 @@ from sklearn.cluster import MiniBatchKMeans
 from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 
+#from setuptools import setup, find_packages
+#setup(name = "kmeansClustering", packages = find_packages())
+
 
 # Settings
 plt.rcParams['figure.figsize'] = (10.0, 8.0)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-# set device
-if torch.cuda.is_available():
-    print('Using GPU')
-    device = torch.device('cuda:0')
-else:
-    print('Using CPU')
-    device = torch.device('cpu')
 
 # Show images
 def show_images(images):
@@ -34,6 +30,15 @@ def show_images(images):
     for index, image in enumerate(images):
         plt.subplot(sqrtn, sqrtn, index+1)
         plt.imshow(image.reshape(28, 28))
+        plt.axis('off')
+
+# Show images
+def show_images2(images):
+    sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
+
+    for index, image in enumerate(images):
+        plt.subplot(sqrtn, sqrtn, index+1)
+        plt.imshow(image.reshape(64, 64))
         plt.axis('off')
 
 
@@ -52,7 +57,7 @@ class AutoEncoder(nn.Module):
             nn.Tanh(),
             nn.Linear(16, 2),
         )
-        
+
         # Decoder
         self.decoder = nn.Sequential(
             nn.Linear(2, 16),
@@ -74,7 +79,6 @@ class AutoEncoder(nn.Module):
 
 # Load model
 model = torch.load('autoencoder.pth')
-model.to('cpu')
 model.eval()
 
 # DataLoader
@@ -89,7 +93,6 @@ test_loader = data.DataLoader(test_set, batch_size=10000, shuffle=False)
 labels = test_set.targets
 unique_labels = len(np.unique(labels))
 
-num_clusters = 10
 
 # Test
 with torch.no_grad():
@@ -100,22 +103,55 @@ with torch.no_grad():
 
         code, outputs = model(inputs)
         
+        x,y = zip(*code)
+        x = np.array(x)
+        y = np.array(y)
+        
+        data = list(zip(x, y))
+        
         # random_state=0 for same seed in kmeans
         #clusters = MiniBatchKMeans(n_clusters=20, n_init='auto',).fit(data)
         clusters = KMeans(n_clusters=10, n_init='auto',).fit(data)
         
+        plt.scatter(x, y, c=clusters.labels_, cmap='rainbow')
+        plt.savefig('Pics/clusters.png')
 
+
+
+#Associates each cluster with most probable label
+# def retrieveInfo(kmeansLabels, train_target_numpyArray):
+#     reference_labels={}
+#     for i in range(len(np.unique(kmeansLabels))):
+#         #If cluster label = i, assign 1. Otherwise assign 0.
+#         index = np.where(kmeansLabels == i,1,0)
+#         num = np.bincount(train_target_numpyArray[index==1]).argmax()
+#         reference_labels[i]=num
+#     return reference_labels
 
 labels = test_set.targets
 
-ref_labels = kmeanFunction.retrieveInfo(clusters.labels_, labels)
-num_predicted = kmeanFunction.assignPredictions(clusters.labels_, ref_labels)
+# def assignPredictions(kmeansLabels, reference_labels):
+#     #Initializes the array - ignore random.rand
+#     predicted_num = np.random.rand(len(kmeansLabels))
 
-accuracy = kmeanFunction.computeAccuracy(num_predicted, labels)
+#     #For picture 0, find corresponding kmeans_label. Then find the actual number that kmeans.label points to.
+#     for i in range(len(kmeansLabels)):
+#         predicted_num[i]=reference_labels[kmeansLabels[i]]
+
+#     return predicted_num
+
+ref_labels = kmeanFunction.retrieveInfo(clusters.labels_, labels)
+predicted_num = kmeanFunction.assignPredictions(clusters.labels_, ref_labels)
+
+# def computeAccuracy(predicted_num, train_target_numpyArray):
+#     accuracy = accuracy_score(predicted_num, train_target_numpyArray)
+#     return accuracy
+
+accuracy = kmeanFunction.computeAccuracy(predicted_num, labels)
 
 print('Ref_labels',ref_labels)
 print('labels',labels[0:20])
-print('num_predicted',num_predicted[0:20])
+print('predicted_num',predicted_num[0:20])
 print('Accuracy',accuracy)
 
 def plot(ref_labels):
@@ -134,4 +170,4 @@ def plot(ref_labels):
     # plt.ylabel("Number")
     # plt.savefig('Pics/numbers.png')
 
-#plot(ref_labels)
+plot(ref_labels)
