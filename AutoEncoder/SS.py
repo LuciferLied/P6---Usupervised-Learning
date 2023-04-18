296db0a82e9e8f397e2c3cdc7690396b36f8daa7
Run396 <Run396@users.noreply.github.com>
2023-04-18 10:58:17 +0200

    Update SS.py

--- AutoEncoder/SS.py
+++ AutoEncoder/SS.py
@@ -6,117 +6,142 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import STL10
 from torchvision.transforms import ToTensor
 
+# set device
+if torch.cuda.is_available():
+    print('Using GPU')
+    dtype = torch.float32
+    device = torch.device('cuda')
+else:
+    print('Using CPU')
+    device = torch.device('cpu')
 
-BATCH_SIZE = 128
+setting = {
+    'epochs': 3,
+    'lr': 0.001,
+    'batch_size': 128,
+}
 
-train_labeled_dataset = STL10(root="data",split="train", download=True, transform=ToTensor())
-train_unlabeled_dataset = STL10(root="data",split="unlabeled", download=True, transform=ToTensor())
-test_labeled_dataset = STL10(root="data",split="test", download=True, transform=ToTensor())
-
-train_labeled_dataloader = DataLoader(
-    dataset=train_labeled_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    drop_last=False,
+train_labeled_dataset = STL10(
+    root="data",
+    split="train",
+    download=True,
+    transform=ToTensor()
 )
-train_unlabeled_dataloader = DataLoader(
-    dataset=train_unlabeled_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    drop_last=False,
+train_unlabeled_dataset = STL10(
+    root="data",
+    split="unlabeled",
+    download=True,
+    transform=ToTensor()
 )
-test_labeled_dataloader = DataLoader(
-    dataset=test_labeled_dataset,
-    batch_size=BATCH_SIZE,
-    drop_last=False,
+test_labeled_dataset = STL10(
+    root="data",
+    split="test",
+    download=True,
+    transform=ToTensor()
 )
 
-device = 'cpu'
+# Train labeled is 5000 images
+# Train shape is (5000, 3, 96, 96)
+train_labeled_dataloader = DataLoader(dataset=train_labeled_dataset,batch_size=setting["batch_size"],shuffle=True) #500 of each class
 
+# Train unlabeled is 100,000 images
+# Train shape is (100000, 3, 96, 96)
+train_unlabeled_dataloader = DataLoader(dataset=train_unlabeled_dataset,batch_size=setting["batch_size"],shuffle=True)
+
+# Test labeled is 8000 images
+# Test shape is (8000, 3, 96, 96)
+test_labeled_dataloader = DataLoader(dataset=test_labeled_dataset,batch_size=setting["batch_size"])
+
+classes = ('airplane', 'bird', 'car', 'cat', 'deer', 'dog', 'horse', 'monkey', 'ship', 'truck')
 
 class SupervisedModel():
     def __init__(self, encoder):
-        self.model = encoder
-        #self.model = encoder.cuda()
+        self.model = encoder.to(device)
 
     @staticmethod
-    def count_correct(
-        y_pred: torch.Tensor,
-        y_true: torch.Tensor
-    ) -> torch.Tensor:
-        """
-        Returns the number of correct predictions
-        """
+    def count_correct(y_pred, y_true):
+        # Returns the number of correct predictions
         preds = torch.argmax(y_pred, dim=1)
         return (preds == y_true).float().sum()
 
-
-    def validate_model(
-        self,
-        loss_fn, 
-        dataloader: DataLoader
-    ):
-        """
-        Validates given model with given loss function on given DataLoader
-        """
+    def validate_model(self,loss_fn, dataloader):
+        # Validates given model with given loss function on given DataLoader
         loss = 0
         correct = 0
         all = 0
         with torch.no_grad():
-            for X_batch, y_batch in dataloader:
-                X_batch, y_batch = X_batch, y_batch
-                #X_batch, y_batch = X_batch.cuda(), y_batch.cuda()
-                y_pred = self.model(X_batch)
+            for pics, labels in dataloader:
+                pics = pics.to(device)
+                labels = labels.to(device)
+                
+                #all is the number of images
+                y_pred = self.model(pics)
                 all += len(y_pred)
-                loss += loss_fn(y_pred, y_batch)
-                correct += SupervisedModel.count_correct(y_pred, y_batch)
+                
+                #loss is the sum of the loss of each image
+                loss += loss_fn(y_pred, labels)
+                
+                #Correct is the number of correct predictions
+                correct += SupervisedModel.count_correct(y_pred, labels)
+                
+        #Return the average loss and the accuracy
         return loss / all, correct / all
 
-
-    def train_model(
-        self,
-        optimiser: optim.Optimizer,
-        loss_fn,
-        train_dl: DataLoader,
-    ):
-        """
-        Trains given model with given loss function on given DataLoader
-        """
+    def train_model(self, optimiser, loss_fn, train_dl):
+        # Trains given model with given loss function on given DataLoader
         self.model.train()
+        
+        # Store the predictions of the model
+        stored = []
+        stored = torch.tensor(stored)
+        
+        for pics, labels in train_dl:
+            print('labels',labels.shape)
+            print('pics',pics.shape)
+            
+            pics = pics.to(device)
+            labels = labels.to(device)
 
-        for X_batch, y_batch in train_dl:
-            X_batch, y_batch = X_batch, y_batch
-            #X_batch, y_batch = X_batch.cuda(), y_batch.cuda()
-            y_pred = self.model(X_batch)
-            loss = loss_fn(y_pred, y_batch)
+            y_pred = self.model(pics)
+            preds = torch.argmax(y_pred, dim=1)
+            print('preds',preds.shape)
 
+            # Zip predictions and pictures together
+            arr = []
+            arr += zip(pics, preds)
+
+            
+   
+            
+            print('arr',arr.shape)
+            for pics, labels in arr:
+                print('labels',labels.shape)
+                print('pics',pics.shape)
+            
+            # loss is the sum of the loss of each image
+            loss = loss_fn(y_pred, labels)
             loss.backward()
             optimiser.step()
             optimiser.zero_grad() 
+            exit()
 
-        self.model.eval() 
+        self.model.eval()
+        print('arr',stored.shape)
 
 
-    def fit(
-        self,
-        loss_fn,
-        train_dl: DataLoader,
-        val_dl: DataLoader,
-        epochs=5
-    ) -> dict:
-        """
-        Trains model and validates on training and validation data
-        """
+    def fit(self, loss_fn, train_dl, test_dl, epochs):
+        
+        # Trains model and validates on training and validation data
         results = {"train_loss": [], "train_acc": [], "val_loss": [],
                    "val_acc": []}
-        optimiser = optim.AdamW(self.model.parameters())  
+        optimiser = optim.AdamW(self.model.parameters())
 
         for epoch in range(epochs):
             start = time.time()
-
+            
             self.train_model(optimiser, loss_fn, train_dl)
-            train_loss, train_acc = self.validate_model(loss_fn, train_dl) 
-            val_loss, val_acc = self.validate_model(loss_fn, val_dl)
+            train_loss, train_acc = self.validate_model(loss_fn, train_dl)
+            val_loss, val_acc = self.validate_model(loss_fn, test_dl)
 
             print(f"Epoch {epoch + 1}: train loss = {train_loss:.3f} "
              f"(acc: {train_acc:.3f}), validation loss = {val_loss:.3f} "
@@ -127,13 +152,44 @@ class SupervisedModel():
             results["val_acc"].append(float(val_acc))
 
         return results
+    
+    # self label
+    def self_label(self, train_dl):
+        # Store the predictions of the model
+        arr = []
+        
+        for pics, labels in train_dl:
+            pics = pics.to(device)
+            labels = labels.to(device)
 
-def evaluate_baseline():
+            y_pred = self.model(pics)
+            
+            print('labels',y_pred.shape)
+            
+            
+        return y_pred
+
+def train_model():
     encoder = torchvision.models.resnet18()
     encoder.fc = nn.Linear(512, 10) # Resnet18 is created for 1000 classses so we need to change the last layer
     baseline_model = SupervisedModel(encoder)
 
     loss_fn = nn.CrossEntropyLoss(reduction='sum')
-    return baseline_model.fit(loss_fn, train_labeled_dataloader, test_labeled_dataloader)
+    # Save
+    torch.save(baseline_model, 'semi.pth')
+        
+    return baseline_model.fit(loss_fn, train_labeled_dataloader, test_labeled_dataloader, setting["epochs"])
 
-history_baseline = evaluate_baseline()
+history_baseline = train_model()
+
+
+def self_label():
+    # Load the model
+    self_label_model = torch.load('semi.pth') 
+    
+    self_label_model.model.eval()
+    self_label_model.model.to(device)
+    self_label_model.self_label(train_unlabeled_dataloader)
+    
+ 
+    return 
