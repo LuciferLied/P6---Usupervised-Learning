5c4bb6b2f24917862a9414d99b6a88e39fd77829
Run396 <Run396@users.noreply.github.com>
2023-04-25 16:10:26 +0200

    Throbbing

--- AutoEncoder/KNN.py
+++ AutoEncoder/KNN.py
@@ -1,19 +1,17 @@
 import torch
 import torch.nn as nn
-from torchvision import datasets
+from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
 import torch.utils.data as data
 import numpy as np
 import matplotlib.pyplot as plt
 from util import Models as Model
-import warnings
-warnings.filterwarnings("ignore")
-
+from util import utils
+from tqdm import tqdm
 
 # set device
 if torch.cuda.is_available():
     print('Using GPU')
-    # dtype = torch.float32
     device = torch.device('cuda')
 else:
     print('Using CPU')
@@ -22,10 +20,41 @@ else:
 # Settings
 setting = {
     'batch_size': 256,
-    'epochs': 14,
+    'epochs': 5,
     'lr': 0.001
 }
 
+from PIL import Image
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+
+
+class CIFAR10Pair(CIFAR10):
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            pos_1 = self.transform(img)
+            pos_2 = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return pos_1, pos_2, target
+
+
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(32),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
 
 # DataLoader
 train_set = datasets.CIFAR10(
@@ -34,43 +63,55 @@ train_set = datasets.CIFAR10(
     download=True,
     transform=ToTensor(),
 )
-
 train_loader = data.DataLoader(train_set, batch_size=setting['batch_size'], shuffle=True)
 
-model = Model.Cifar_AutoEncoder()
+train_data = utils.CIFAR10Pair(root='data', train=True, transform=utils.train_transform, download=True)
+augmeted_loader = data.DataLoader(train_data, batch_size=setting['batch_size'], shuffle=True, pin_memory=True,drop_last=True)
+
+model = Model.simCLR()
 model.to(device)
 
 # Optimizer and loss function
-optimizer = torch.optim.Adam(model.parameters(), lr=setting['lr'])
+optimizer = torch.optim.Adam(model.parameters(), lr=setting['lr'],weight_decay=1e-5)
 criteria = nn.MSELoss()
 
+temperature = 0.5
 # Train
 def train():
     for epoch in range(setting['epochs']):
-        for pics, labels in train_loader:
-            pics = pics.to(device)
+        total_loss, total_num, train_bar = 0.0, 0, tqdm(augmeted_loader)
+        for pics1, pics2, labels in train_bar:
+            pics1 = pics1.to(device)
+            pics2 = pics2.to(device)
 
             # Forward
-            codes, recon = model(pics)
+            codes, out_1 = model(pics1)
+            codes, out_2 = model(pics2)
+            
+            out_1 = torch.flatten(out_1, start_dim=1)
+            out_2 = torch.flatten(out_2, start_dim=1)
 
+            out = torch.cat([out_1, out_2], dim=0)
+            # [2*B, 2*B]
+            sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+            mask = (torch.ones_like(sim_matrix) - torch.eye(2 * setting['batch_size'], device=sim_matrix.device)).bool()
+            # [2*B, 2*B-1]
+            sim_matrix = sim_matrix.masked_select(mask).view(2 * setting['batch_size'], -1)
+
+            # compute loss
+            pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+            # [2*B]
+            pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+            loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+            
             # Backward
             optimizer.zero_grad()
-            loss = criteria(recon, pics)            
             loss.backward()
             optimizer.step()
             
-            save_pic = pics
-            save_dec = recon
-        
-        # Show progress
-        print('[{}/{}] Loss:'.format(epoch+1, setting['epochs']), loss.item())
-
-        plt.imshow(save_pic[1].cpu().squeeze().numpy().transpose(1, 2, 0))
-        plt.savefig('pics/original.png')
-        
-        save_dec = save_dec[1].view(-1, 3, 32, 32)
-        plt.imshow(save_dec.cpu().squeeze().detach().numpy().transpose(1, 2, 0))
-        plt.savefig('pics/reconstructed.png')
+            total_num += setting['batch_size']
+            total_loss += loss.item() * setting['batch_size']
+            train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, setting['epochs'], total_loss / total_num))
 
 train()
 
--- AutoEncoder/KNN_test.py
+++ AutoEncoder/KNN_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.cluster import KMeans
 from sklearn.model_selection import GridSearchCV
 import torch
 from torchvision import datasets
@@ -8,15 +9,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score,confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+from util import utils as util
+from util import Models as Model
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
-import warnings
-warnings.filterwarnings("ignore")
+import time
 
+torch.manual_seed(0)
 
 # set device
 if torch.cuda.is_available():
     print('Using GPU')
-    dtype = torch.float32
     device = torch.device('cuda')
 else:
     print('Using CPU')
@@ -37,8 +39,6 @@ train_set = datasets.CIFAR10(
     transform=ToTensor(),
 )
 
-train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=False)
-
 test_set = datasets.CIFAR10(
     root="data",
     train=False,
@@ -46,93 +46,65 @@ test_set = datasets.CIFAR10(
     transform=ToTensor(),
 )
 
+train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=False)
+knn_train_loader = data.DataLoader(train_set, batch_size=5000, shuffle=False)
+
 test_loader = data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+knn_test_loader = data.DataLoader(test_set, batch_size=5000, shuffle=False)
 
-#Classes
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
+images, labels = next(iter(train_loader))
+print(images.shape)
 
 def KNN(train_data,train_labs,test_data,test_labs):
-
-    print('train_data shape: ',train_data.shape)
-    print('train_labs shape: ',train_labs.shape)
-    print('test_data shape: ',test_data.shape)
-    print('test_labs shape: ',test_labs.shape)
+    # Log time
+    start_time = time.time()
     
-    KNN = KNeighborsClassifier()
+    KNN = KNeighborsClassifier(n_neighbors=200)
+    
     scaler = StandardScaler()
-    
     train_data = scaler.fit_transform(train_data)
     test_data = scaler.fit_transform(test_data)
-
+    
+    
     KNN.fit(train_data,train_labs)
     predicted = KNN.predict(test_data)
 
-
     #In order to pretty print output
-    print('Accuracy is :',accuracy_score(predicted,test_labs)*100,'%')
+    print('KNN Accuracy',accuracy_score(predicted,test_labs)*100,'%')
 
-    #List Hyperparameters to tune
-    leaf_size = list(range(1,5))
-    n_neighbors = list(range(1,8))
-    p=[1,2]
-    #convert to dictionary
-    hyperparameters = dict(leaf_size=leaf_size, n_neighbors=n_neighbors, p=p)
-    #Making model
-    clf = GridSearchCV(KNN, hyperparameters, cv=10)
-    best_model = clf.fit(train_data,train_labs)
-    #Best Hyperparameters Value
-    print('Best leaf_size:', best_model.best_estimator_.get_params()['leaf_size'])
-    print('Best p:', best_model.best_estimator_.get_params()['p'])
-    print('Best n_neighbors:', best_model.best_estimator_.get_params()['n_neighbors'])
-    #Predict testing set
-    y_pred = best_model.predict(test_data)
-    #Check performance using accuracy
-    print(accuracy_score(test_labs, y_pred))
+    # #List Hyperparameters to tune
+    # leaf_size = list(range(1,2))
+    # n_neighbors = list(range(1,8))
+    # p=[1,2]
+    # #convert to dictionary
+    # hyperparameters = dict(leaf_size=leaf_size, n_neighbors=n_neighbors, p=p)
+    # #Making model
+    # clf = GridSearchCV(KNN, hyperparameters, cv=10)
+    # best_model = clf.fit(train_data,train_labs)
+    # #Best Hyperparameters Value
+    # print('Best leaf_size:', best_model.best_estimator_.get_params()['leaf_size'])
+    # print('Best p:', best_model.best_estimator_.get_params()['p'])
+    # print('Best n_neighbors:', best_model.best_estimator_.get_params()['n_neighbors'])
+    # #Predict testing set
+    # y_pred = best_model.predict(test_data)
+    # #Check performance using accuracy
+    # print(accuracy_score(test_labs, y_pred))
     
-    confmatrix = confusion_matrix(predicted,test_labs)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    # # confmatrix = confusion_matrix(predicted,test_labs)
 
-    plt.subplots(figsize=(6,6))
-    sns.heatmap(confmatrix,annot=True,fmt=".1f",linewidths=1.5)
-    plt.savefig('confusion_matrix.png')
+    # plt.subplots(figsize=(6,6))
+    # sns.heatmap(confmatrix,annot=True,fmt=".1f",linewidths=1.5)
+    # plt.savefig('confusion_matrix.png')
 
 def test_data():
-    with torch.no_grad():
-        train_data = torch.tensor([])
-        train_labs = torch.tensor([])
-        test_labs = torch.tensor([])
-        test_data = torch.tensor([])
 
-        for train_pics, train_labels in train_loader:
-            train_pics = train_pics.to(device)
-            train_codes, _ = model(train_pics)
-            
-            train_codes = train_codes.to('cpu')
-            train_codes = torch.flatten(train_codes, 1)
-            
-            train_data = torch.cat((train_data, train_codes), 0)
-            train_labs = torch.cat((train_labs, train_labels), 0)
-            if len(train_data) > 5000:
-                break
-        
-    with torch.no_grad():
-        for test_pics, test_labels in test_loader:
-            test_pics = test_pics.to(device)
-            test_codes, _ = model(test_pics)
-            
-            test_codes = test_codes.to('cpu')
-            test_codes = torch.flatten(test_codes, 1)
-            
-            test_data = torch.cat((test_data, test_codes), 0)
-            test_labs = torch.cat((test_labs, test_labels), 0)
-            if len(test_data) > 5000:
-                break
-
-        # Remove last element
-        train_data = train_data[:-1]
-        train_labs = train_labs[:-1]
-
-        
-    KNN(train_data, train_labs, test_data, test_labs)
+    train_imgs, train_labs = next(iter(knn_train_loader))
+    test_imgs , test_labs  = next(iter(knn_test_loader))
+    
+    train_codes, _ = model(train_imgs.to(device))
+    test_codes, _  = model(test_imgs.to(device))
+    
+    KNN(train_codes.flatten(1).detach().cpu().numpy(), train_labs, test_codes.flatten(1).detach().cpu().numpy(), test_labs)
 
 test_data()
