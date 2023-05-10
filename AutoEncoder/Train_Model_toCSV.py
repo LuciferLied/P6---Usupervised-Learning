82111626007a878749c53756c3406b7f64c7c61d
Run396 <Run396@users.noreply.github.com>
2023-05-10 09:51:38 +0200

    Update Train_Model_toCSV.py

--- AutoEncoder/Train_Model_toCSV.py
+++ AutoEncoder/Train_Model_toCSV.py
@@ -30,16 +30,24 @@ def load_data(data_set, batch_size):
                 target = self.target_transform(target)
 
             return pos_1, pos_2, target
-
-    train_transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.RandomResizedCrop(32),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-        transforms.RandomGrayscale(p=0.2),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-    ])
+    
+    
+    if data_set.__name__ == 'CIFAR10':
+        train_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomResizedCrop(32),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+        ])
+    if data_set.__name__ == 'MNIST':
+        train_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ToTensor(),
+        ])
     
     test_transform = transforms.Compose([
         transforms.ToTensor(),
@@ -60,18 +68,21 @@ def load_data(data_set, batch_size):
 def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size, lr, load, neighbors_cluster):
     
     if load == True:
-        load_model = 'trained_models/chk/Res18_CIFAR10_30_0.001.pth'
+        load_model = 'trained_models/chk/simCLR_CIFAR10_20_512_0.001.pth'
         model = torch.load(load_model, map_location=torch.device(device))
         temp = load_model.split('/')
-        vars = temp[1].split('_')
+        vars = temp[2].split('_')
+        print(vars)
         name = vars[0]
         data_name = vars[1]
         pretrained_epochs = vars[2]
         pretrained_epochs = int(pretrained_epochs)
+        batch_size = vars[3]
         print('Model:', name,'pretrained with:', data_name, 'epochs:', pretrained_epochs, 'lr:', lr)
         lr = vars[3].replace('.pth', '')
         lr = float(lr)
     else:
+        print('data_name:', data_name)
         model = Model.simCLR(data_name)
         pretrained_epochs = 0
 
@@ -117,7 +128,6 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
             _, out_2 = model(pics2)
             
             loss = contrastive_loss(out_1, out_2, temperature)
-            
             # Backward
             optimizer.zero_grad()
             loss.backward()
@@ -127,10 +137,10 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
             total_loss += loss.item() * batch_size
             train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch + pretrained_epochs + 1, epochs + pretrained_epochs, total_loss / total_num))
         #save model for loading later
-        if ((epoch + 1) % 5 == 0) or (epoch == epochs - 1):
+        if ((epoch + 1) % 10 == 0) or (epoch == epochs - 1):
             print('Saving model as: ', 'trained_models/chk{}_{}_{}_{}_{}.pth'.format(name, data_name, epoch + 1 + pretrained_epochs, batch_size, lr))
             saveAsChkPnt = 'trained_models/chk/{}_{}_{}_{}_{}.pth'.format(name, data_name, epoch + 1 + pretrained_epochs, batch_size, lr)
-            torch.save(model.state_dict(), saveAsChkPnt)
+            torch.save(model, saveAsChkPnt)
         
         if ((epoch + 1) % 5 == 0) or (epoch == epochs - 1):
             knn_accuracy, kmeans_accuracy = test(model, aug_test_train, aug_test_test, device, neighbors_cluster, False)
@@ -141,12 +151,12 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
 
 #Settings
 total_epochs = 30
-batch_sizes = [256, 512, 1024, 2048]
+batch_sizes = [512]
 learns = [0.001]
-neighbors_cluster = 200
+neighbors_cluster = 20
 
 
-data_set = CIFAR10
+data_set = MNIST
 data_name = data_set.__name__
 
 load = False
@@ -157,5 +167,5 @@ for size in batch_sizes:
         train(data_name ,aug_train, aug_test_train, aug_test_test, total_epochs, size, lr, load, neighbors_cluster)
 
 
-model = torch.load('trained_models/simCLR_CIFAR10_70_256_0.001.pth', map_location=torch.device(device))
-knn_accuracy, kmeans_accuracy = test(model, aug_test_train, aug_test_test, device, neighbors_cluster, True)
+# model = torch.load('trained_models/simCLR_CIFAR10_70_256_0.001.pth', map_location=torch.device(device))
+# knn_accuracy, kmeans_accuracy = test(model, aug_test_train, aug_test_test, device, neighbors_cluster, True)
