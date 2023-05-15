49f6ebab7bdbde7d7c73116c0ae650bc7c154aa0
Run396 <Run396@users.noreply.github.com>
2023-05-15 15:26:42 +0200

    Update Train_Model_toCSV.py

--- AutoEncoder/Train_Model_toCSV.py
+++ AutoEncoder/Train_Model_toCSV.py
@@ -9,7 +9,7 @@ import torch.utils.data as data
 
 # set device
 if torch.cuda.is_available():
-    #print('Using GPU')
+    print('Using GPU')
     dtype = torch.float32
     device = torch.device('cuda')
 else:
@@ -42,17 +42,22 @@ def load_data(data_set, batch_size):
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
         ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+        ])
+        
     if data_set.__name__ == 'MNIST':
         train_transform = transforms.Compose([
             transforms.ToPILImage(),
+            transforms.RandomResizedCrop(32),
+            transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor(),
         ])
-    
-    test_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-    ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
 
     train_data = AUG_PAIR(root='data', train=True, transform=train_transform, download=True)
     aug_train = data.DataLoader(train_data, batch_size=batch_size, shuffle=True, pin_memory=True,drop_last=True)
@@ -83,7 +88,7 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
         lr = float(lr)
     else:
         print('data_name:', data_name)
-        model = Model.simCLR(data_name)
+        model = Model.simCLRConv(data_name)
         pretrained_epochs = 0
 
     model.to(device)
@@ -96,23 +101,26 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=1e-6)
 
     temperature = 0.5
-
+    
     def contrastive_loss(out_1, out_2, temperature):
         out_1 = torch.flatten(out_1, start_dim=1)
         out_2 = torch.flatten(out_2, start_dim=1)
 
         out = torch.cat([out_1, out_2], dim=0)
-        # [2*B, 2*B]
-        sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
-        mask = (torch.ones_like(sim_matrix) - torch.eye(2 * batch_size, device=sim_matrix.device)).bool()
-        # [2*B, 2*B-1]
-        sim_matrix = sim_matrix.masked_select(mask).view(2 * batch_size, -1)
+        
+        # Full Similiraty Matrix
+        cov = torch.mm(out, out.t().contiguous())
+        sim = torch.exp(cov / temperature)
+        
+        # Negative Similarity Matrix
+        mask = (torch.ones_like(sim) - torch.eye(2 * batch_size, device=sim.device)).bool()
+        neg = sim.masked_select(mask).view(2 * batch_size, -1)
 
-        # compute loss
-        pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
-        # [2*B]
-        pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
-        loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+        # Positive Similarity Matrix
+        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+        pos = torch.cat([pos, pos], dim=0)
+        
+        loss = -torch.log(pos / neg.sum(dim=-1)).mean()
 
         return loss
 
@@ -122,7 +130,7 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
         for pics1, pics2, labels in train_bar:
             pics1 = pics1.to(device)
             pics2 = pics2.to(device)
-
+            
             # Forward
             _, out_1 = model(pics1)
             _, out_2 = model(pics2)
@@ -136,6 +144,7 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
             total_num += batch_size
             total_loss += loss.item() * batch_size
             train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch + pretrained_epochs + 1, epochs + pretrained_epochs, total_loss / total_num))
+            
         #save model for loading later
         if ((epoch + 1) % 10 == 0) or (epoch == epochs - 1):
             print('Saving model as: ', 'trained_models/chk{}_{}_{}_{}_{}.pth'.format(name, data_name, epoch + 1 + pretrained_epochs, batch_size, lr))
@@ -151,12 +160,12 @@ def train(data_name,aug_train, aug_test_train, aug_test_test, epochs, batch_size
 
 #Settings
 total_epochs = 30
-batch_sizes = [512]
+batch_sizes = [256]
 learns = [0.001]
 neighbors_cluster = 20
 
 
-data_set = MNIST
+data_set = CIFAR10
 data_name = data_set.__name__
 
 load = False
