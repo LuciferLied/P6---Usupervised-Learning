3f07f85329b5e1484f9f3090f5d7ee52eba7cbb9
Run396 <Run396@users.noreply.github.com>
2023-05-16 14:40:46 +0200

    New tests

--- AutoEncoder/Train_Model.py
+++ AutoEncoder/Train_Model.py
@@ -1,16 +1,20 @@
-# https://clay-atlas.com/us/blog/2021/08/03/machine-learning-en-introduction-autoencoder-with-pytorch/
 import torch
 import torch.nn as nn
 import torch.utils.data as data
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-import torchvision.transforms as T
-import time 
+import time
+from tqdm import tqdm 
 from util import Models as Model
-import matplotlib.pyplot as plt
-import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
+import torch
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from util import utils
 
 start = time.time()
 # set device
@@ -23,23 +27,34 @@ else:
     device = torch.device('cpu')
 
 # Settings
-epochs = 5
+epochs = 30
 batch_size = 256
 lr = 0.001
+neighbors_cluster = 20
 
 # DataLoader
-train_set = datasets.CIFAR10(
+train_set = datasets.MNIST(
     root="data",
     train=True,
     download=True,
     transform=ToTensor(),
 )
 
-train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=True,drop_last=True)
 
+# DataLoader
+test_set = datasets.MNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
 
+test_loader = data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+name = train_set.__class__.__name__
 # Optimizer and loss function
-model = Model.Cifar_AutoEncoder()
+model = Model.AE_ResNet(name)
 print(model)
 
 model.to(device)
@@ -48,19 +63,17 @@ loss_function = nn.MSELoss()
 
 # Train
 for epoch in range(epochs):
-    for pics, labels in train_loader:
+    total_loss, total_num, train_bar = 0.0, 0, tqdm(train_loader)
+    for pics, labels in train_bar:
         pics = pics.to(device)
-        pics1 = pics
         
-        #greyscale pics
-        # pics = T.Grayscale()(pics)
-        # pics = torch.flatten(pics, 1)
-
         # Forward
         codes, decoded = model(pics)
 
-        decoded1 = decoded
-        # decoded = torch.flatten(decoded, 1)
+        if name == 'CIFAR10':
+            decoded = decoded.view(-1, 3, 32, 32)
+        if name == 'MNIST':
+            decoded = decoded.view(-1, 1, 28, 28)
         # Backward
         optimizer.zero_grad()
         
@@ -68,22 +81,77 @@ for epoch in range(epochs):
         
         loss.backward()
         optimizer.step()
-    print('codes', codes.shape)
-    # Show progress
-    print('[{}/{}] Loss:'.format(epoch+1, epochs), loss.item())
+        total_num += batch_size
+        total_loss += loss.item() * batch_size
+        # Show progress
+        train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch + 1, epochs, total_loss / total_num))
+    
+def KNN(train_data, train_labs, test_data, test_labs):
+    
+    KNN = KNeighborsClassifier(n_neighbors=20)
+    scaler = StandardScaler()
+    train_data = scaler.fit_transform(train_data)
+    test_data = scaler.fit_transform(test_data)
 
-    plt.imshow(pics1[1].cpu().squeeze().numpy().transpose(1, 2, 0))
-    # plt.imshow(pics1[1].cpu().squeeze().numpy())
-    plt.savefig('pics/original.png')
-    decoded1 = decoded1[1].view(-1, 3, 32, 32)
-    plt.imshow(decoded1.cpu().squeeze().detach().numpy().transpose(1, 2, 0))
-    # plt.imshow(decoded1.cpu().squeeze().detach().numpy())
-    plt.savefig('pics/reconstructed.png')
+    KNN.fit(train_data, train_labs)
+    predicted = KNN.predict(test_data)
+    
+    accuracy = accuracy_score(predicted, test_labs)
+    
+    return accuracy*100
+
+def kmeans(train_data, train_labs):
+    scaler = StandardScaler()
+    train_data = scaler.fit_transform(train_data)
+    kmeans = KMeans(n_clusters=20, n_init='auto').fit(train_data)
+    
+    labels = train_labs
+    # cast to int
+    labels = np.array(labels)
+    labels = labels.astype(int)
+    
+    labels = np.array(labels)
+    ref_labels = utils.retrieveInfo(kmeans.labels_, labels)
+    num_predicted = utils.assignPredictions(kmeans.labels_, ref_labels)
+    accuracy = utils.computeAccuracy(num_predicted, labels)
+
+    return accuracy*100
+
+def test(model, train_loader, test_loader, device):
+    with torch.no_grad():
+        train_codes = torch.tensor([])
+        train_labs = torch.tensor([])
+        test_codes = torch.tensor([])
+        test_labs = torch.tensor([])
+        
+        print('loading test...')
+        
+        for pics, labels in train_loader:
+            codes, _ = model(pics.to(device))
+            train_codes = torch.cat((train_codes, codes.flatten(1).cpu()), 0)
+            train_labs = torch.cat((train_labs, labels), 0)
+            if len(train_codes) > 9000:
+                break
+
+        for pics,labels in test_loader:
+            codes, _ = model(pics.to(device))
+            test_codes = torch.cat((test_codes, codes.flatten(1).cpu()), 0)
+            test_labs = torch.cat((test_labs, labels), 0)
+            if len(test_codes) > 9000:
+                break
+        
+        print('test loaded')
+        print('testing...')
+        
+    knn_acc = KNN(train_codes, train_labs, test_codes, test_labs)
+    kmeans_acc = kmeans(train_codes,train_labs)
+    
+    return knn_acc, kmeans_acc
 
 
 
-print('Finished Training using', device)
+knn_acc, kmeans_acc = test(model, train_loader, test_loader, device)
+print('KNN Accuracy: {:.2f} %'.format(knn_acc))
+print('KMeans Accuracy: {:.2f} %'.format(kmeans_acc))
+
 print('Time: ', time.time() - start)
-
-# Save
-torch.save(model, 'autoencoder.pth')
--- AutoEncoder/util/Models.py
+++ AutoEncoder/util/Models.py
@@ -4,132 +4,153 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models.resnet import resnet18
 from torchvision.models.mobilenetv2 import mobilenet_v2
-from torchvision.models.squeezenet import squeezenet1_1
 
 
-class AutoEncoder(nn.Module):
-    def __init__(self):
-        super(AutoEncoder, self).__init__()
-        
-        # Encoder
-        self.encoder = nn.Sequential(
-            nn.Linear(784, 128),
-            nn.Tanh(),
-            nn.Linear(128, 64),
-            nn.Tanh(),
-            nn.Linear(64, 16),
-            nn.Tanh(),
-            nn.Linear(16, 2)
-        )
-        
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(2, 16),
-            nn.Tanh(),
-            nn.Linear(16, 64),
-            nn.Tanh(),
-            nn.Linear(64, 128),
-            nn.Tanh(),
-            nn.Linear(128, 784),
-            nn.Sigmoid()
-        )
+class AE(nn.Module):
+    def __init__(self, dataset):
+        super(AE, self).__init__()
 
+        if dataset == 'CIFAR10':
+            # Encoder
+            self.encoder = nn.Sequential(
+                nn.Linear(3072, 128),
+                nn.Tanh(),
+                nn.Linear(128, 64)
+            )
+            
+            # Decoder
+            self.decoder = nn.Sequential(
+                nn.Linear(64, 128),
+                nn.Tanh(),
+                nn.Linear(128, 3072),
+                nn.Sigmoid()
+            )
+        if dataset == 'MNIST':
+            # Encoder
+            self.encoder = nn.Sequential(
+                nn.Linear(784, 128),
+                nn.Tanh(),
+                nn.Linear(128, 64)
+            )
+            
+            # Decoder
+            self.decoder = nn.Sequential(
+                nn.Linear(64, 128),
+                nn.Tanh(),
+                nn.Linear(128, 784),
+                nn.Sigmoid()
+            )
     def forward(self, inputs):
-        
+        inputs = inputs.flatten(start_dim=1)
         codes = self.encoder(inputs)
         decoded = self.decoder(codes)
         
         return codes, decoded
 
-class Smol_AutoEncoder(nn.Module):
-    def __init__(self):
-        super(Smol_AutoEncoder, self).__init__()
+class AE_Conv(nn.Module):
+    def __init__(self, dataset):
+        super(AE_Conv, self).__init__()
 
         # Encoder
-        self.encoder = nn.Sequential(
-            nn.Linear(784, 128),
-            nn.Tanh(),
-            nn.Linear(128, 64)
-        )
-        
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(64, 128),
-            nn.Tanh(),
-            nn.Linear(128, 784),
-            nn.Sigmoid()
-        )
+        if dataset == 'CIFAR10':
+            self.encoder = nn.Sequential(
+                nn.Conv2d(3, 32, 32, padding='same'),
+                nn.ReLU(),
+                nn.BatchNorm2d(32),
+                nn.Conv2d(32, 32, 3, stride=2, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(32, 32, 3, padding='same'),
+                nn.ReLU(),
+                nn.BatchNorm2d(32),
+                nn.Conv2d(32, 8, 3),
+                nn.AvgPool2d(2, 2)
+            )
+            
+            # Decoder
+            self.decoder = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode='nearest'),
+                nn.ConvTranspose2d(8, 32, 3),
+                nn.Upsample(scale_factor=2, mode='nearest'),
+                nn.Conv2d(32, 32, 3, padding='same'),
+                nn.ReLU(),
+                nn.BatchNorm2d(32),
+                nn.Conv2d(32, 3, 3, padding='same'),
+                nn.Sigmoid()
+            )
+        if dataset == 'MNIST':
+            self.encoder = nn.Sequential(
+                nn.Conv2d(1, 32, 32, padding='same'),
+                nn.ReLU(),
+                nn.BatchNorm2d(32),
+                nn.Conv2d(32, 32, 3, stride=2, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(32, 32, 3, padding='same'),
+                nn.ReLU(),
+                nn.BatchNorm2d(32),
+                nn.Conv2d(32, 8, 3),
+                nn.AvgPool2d(2, 2)
+            )
+            
+            # Decoder
+            self.decoder = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode='nearest'),
+                nn.ConvTranspose2d(8, 32, 3),
+                nn.Upsample(scale_factor=2, mode='nearest'),
+                nn.Conv2d(32, 32, 3, padding='same'),
+                nn.ReLU(),
+                nn.BatchNorm2d(32),
+                nn.Conv2d(32, 1, 3, padding='same'),
+                nn.Sigmoid()
+            )
 
     def forward(self, inputs):
-        
         codes = self.encoder(inputs)
         decoded = self.decoder(codes)
         
         return codes, decoded
+    
+# AE with ResNet18 as base encoder
+class AE_ResNet(nn.Module):
+    def __init__(self, dataset):
+        super(AE_ResNet, self).__init__()
 
-class Cifar_AutoEncoder(nn.Module):
-    def __init__(self):
-        super(Cifar_AutoEncoder, self).__init__()
+        if dataset == 'CIFAR10':
+            self.encoder = []
+            for name, module in resnet18().named_children():
+                if name == 'conv1':
+                    module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+                if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
+                    self.encoder.append(module)
+                    
+            self.encoder = nn.Sequential(*self.encoder)
+            self.decoder = nn.Sequential(
+                nn.LazyLinear(512),
+                nn.ReLU(),
+                nn.BatchNorm1d(512),
+                nn.LazyLinear(3072),
+            )
+        if dataset == 'MNIST':
+            self.encoder = []
+            for name, module in resnet18().named_children():
+                if name == 'conv1':
+                    module = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+                if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
+                    self.encoder.append(module)
+                    
+            self.encoder = nn.Sequential(*self.encoder)
+            self.decoder = nn.Sequential(
+                nn.LazyLinear(512),
+                nn.ReLU(),
+                nn.BatchNorm1d(512),
+                nn.LazyLinear(784),
+            )
 
-        # Encoder
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, 32, padding='same'),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, 3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, 3, padding='same'),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 8, 3),
-            nn.AvgPool2d(2, 2)
-        )
-        
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='nearest'),
-            nn.ConvTranspose2d(8, 32, 3),
-            nn.Upsample(scale_factor=2, mode='nearest'),
-            nn.Conv2d(32, 32, 3, padding='same'),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 3, 3, padding='same'),
-            nn.Sigmoid()
-        )
-        
     def forward(self, inputs):
         codes = self.encoder(inputs)
+        codes = torch.flatten(codes, start_dim=1)
         decoded = self.decoder(codes)
-        
         return codes, decoded
-
-class Res18(nn.Module):
-    def __init__(self, feature_dim=128):
-        super(Res18, self).__init__()
-
-        self.encoder = []
-        for name, module in resnet18().named_children():
-            if name == 'conv1':
-                module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-            if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
-                self.encoder.append(module)
-
-        # encoder
-        self.encoder = nn.Sequential(*self.encoder)
-        # projection head
-        self.g = nn.Sequential(
-            nn.Linear(512, 512, bias=False),
-            nn.BatchNorm1d(512),
-            nn.ReLU(inplace=True),
-            nn.Linear(512, feature_dim, bias=True)
-        )
-
-    def forward(self, x):
-        
-        x = self.encoder(x)
-        feature = torch.flatten(x, start_dim=1)
-        out = self.g(feature)
-        return F.normalize(feature, dim=-1), F.normalize(out, dim=-1)
+    
     
 class simCLR(nn.Module):
     def __init__(self, dataset, feature_dim=128):
@@ -161,7 +182,6 @@ class simCLR(nn.Module):
         )
 
     def forward(self, x):
-        
         x = self.encoder(x)
         feature = torch.flatten(x, start_dim=1)
         out = self.g(feature)
@@ -184,29 +204,42 @@ class simCLRMobile(nn.Module):
         codes = torch.flatten(x, 1)
         out = self.projection_head(codes)
         return F.normalize(codes,dim=1), F.normalize(out, dim=1)    
-    
-# simCLR with Linear as base encoder
-class simCLRLinear(nn.Module):
-    def __init__(self):
+
+class simCLRConv(nn.Module):
+    def __init__(self, dataset, feature_dim=128):
         super().__init__()
-        self.base_encoder = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(3*32*32, 1000),
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 32, 32, padding='same'),
             nn.ReLU(),
-            nn.Linear(1000, 512),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, 3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU()
+            nn.Conv2d(32, 32, 3, padding='same'),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, padding='same'),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, padding='same'),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 14, 3),
+            nn.AvgPool2d(2, 2)
         )
+        
         self.projection_head = nn.Sequential(
-            nn.Linear(512, 512, bias=False),
+            nn.LazyLinear(512, bias=False),
             nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 128)
+            nn.Linear(512, feature_dim, bias=True)
         )
     
     def forward(self, x):
-        x = self.base_encoder(x)
+        x = self.encoder(x)
         codes = torch.flatten(x, 1)
         out = self.projection_head(codes)
         return F.normalize(codes,dim=1), F.normalize(out, dim=1)
